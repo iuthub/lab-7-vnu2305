c47cfe1f8a5a4582148029c488cf0cc666f05ca3
vnu2305 <70842872+vnu2305@users.noreply.github.com>
2021-04-13 01:15:40 +0500

    e

--- webpage/index.php
+++ webpage/index.php
@@ -1,5 +1,46 @@
 <?php
 include('connection.php');
+session_start();
+
+
+if (isset($_GET['logout'])&&$_GET['logout']=='1')
+{
+        session_unset();
+        session_destroy();
+}
+
+$db = new PDO('mysql:host=localhost;dbname=blog', 'vnu', 'xTKBw1M1tDPMuG/E');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$get_accounts_stmt = $db->prepare('SELECT username,password FROM users');
+$get_accounts_stmt->execute();
+$rows = $get_accounts_stmt->fetchAll();
+
+
+$username = '';
+$password = '';
+$rememberME = isset($_POST['remember'])?$_POST['remember']:'';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $authName = $_POST["username"];
+    $authPwd = $_POST["pwd"];
+
+    foreach ($rows as $row) {
+    if ($row["username"] == $authName && $row["password"] == $authPwd) {
+        $_SESSION['username'] = $row['username'];
+        $_SESSION['password'] = $row['password'];
+    }
+    }
+    if($rememberME)
+    {
+        setcookie("username",$_POST["username"],time()+60*60*24*365);
+        setcookie("password",$_POST["pwd"],time()+60*60*24*365);
+    }
+    else
+    {
+        setcookie("username",$_POST["username"],time()+-1);
+        setcookie("password",$_POST["pwd"],time()-1);
+    }
+}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -10,6 +51,9 @@ include('connection.php');
 	
 	<body>
 		<?php include('header.php'); ?>
+        <?php
+        if(!isset($_SESSION['username']))
+        { ?>
 		<!-- Show this part if user is not signed in yet -->
 		<div class="twocols">
 			<form action="index.php" method="post" class="twocols_col">
@@ -24,7 +68,7 @@ include('connection.php');
 					</li>
 					<li>
 						<label for="remember">Remember Me</label>
-						<input type="checkbox" name="remember" id="remember" checked />
+						<input type="checkbox" name="remember" id="remember" value="true" checked />
 					</li>
 					<li>
 						<input type="submit" value="Submit" /> &nbsp; Not registered? <a href="register.php">Register</a>
@@ -36,38 +80,42 @@ include('connection.php');
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur libero nostrum consequatur dolor. Nesciunt eos dolorem enim accusantium libero impedit ipsa perspiciatis vel dolore reiciendis ratione quam, non sequi sit! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio nobis vero ullam quae. Repellendus dolores quis tenetur enim distinctio, optio vero, cupiditate commodi eligendi similique laboriosam maxime corporis quasi labore!</p>
 			</div>
 		</div>
-		
-		<!-- Show this part after user signed in successfully -->
-		<div class="logout_panel"><a href="register.php">My Profile</a>&nbsp;|&nbsp;<a href="index.php?logout=1">Log Out</a></div>
-		<h2>New Post</h2>
-		<form action="index.php" method="post">
-			<ul class="form">
-				<li>
-					<label for="title">Title</label>
-					<input type="text" name="title" id="title" />
-				</li>
-				<li>
-					<label for="body">Body</label>
-					<textarea name="body" id="body" cols="30" rows="10"></textarea>
-				</li>
-				<li>
-					<input type="submit" value="Post" />
-				</li>
-			</ul>
-		</form>
-		<div class="onecol">
-			<div class="card">
-				<h2>TITLE HEADING</h2>
-				<h5>Author, Sep 2, 2017</h5>
-				<p>Some text..</p>
-				<p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-			</div>
-			<div class="card">
-				<h2>TITLE HEADING</h2>
-				<h5>Author, Sep 2, 2017</h5>
-				<p>Some text..</p>
-				<p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-			</div>
-		</div>
+		<?php } ?>
+        <?php
+        if(isset($_SESSION['username']))
+        { ?>
+            <!-- Show this part after user signed in successfully -->
+            <div class="logout_panel"><a href="register.php">My Profile</a>&nbsp;|&nbsp;<a href="index.php?logout=1">Log Out</a></div>
+            <h2 class="newPost">New Post</h2>
+            <form action="index.php" method="post">
+                <ul class="form">
+                    <li>
+                        <label for="title">Title</label>
+                        <input type="text" name="title" id="title" />
+                    </li>
+                    <li>
+                        <label for="body">Body</label>
+                        <textarea name="body" id="body" cols="30" rows="10"></textarea>
+                    </li>
+                    <li>
+                        <input type="submit" value="Post" />
+                    </li>
+                </ul>
+            </form>
+            <div class="onecol">
+                <div class="card">
+                    <h2>TITLE HEADING</h2>
+                    <h5>Author, Sep 2, 2017</h5>
+                    <p>Some text..</p>
+                    <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                </div>
+                <div class="card">
+                    <h2>TITLE HEADING</h2>
+                    <h5>Author, Sep 2, 2017</h5>
+                    <p>Some text..</p>
+                    <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                </div>
+            </div>
+        <?php } ?>
 	</body>
 </html>
--- webpage/register.php
+++ webpage/register.php
@@ -1,7 +1,35 @@
 <?php  
 
 include('connection.php');
+$db = new PDO('mysql:host=localhost;dbname=blog', 'vnu', 'xTKBw1M1tDPMuG/E');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$add_account_stmt = $db->prepare('INSERT INTO users(username, fullname, email,password) VALUES(?,?,?,?)');
 
+
+$notConfirmed = false;
+$username = '';
+$fullName = '';
+$password = '';
+$password2 = '';
+$email = '';
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $username = $_REQUEST['username'];
+    $fullName = $_REQUEST['fullname'];
+    $email = $_REQUEST['email'];
+    $password = $_REQUEST['pwd'];
+    $password2 = $_REQUEST['confirm_pwd'];
+    $passwordIsConfirmed = $password==$password2;
+    if(!$passwordIsConfirmed)
+    {
+        $notConfirmed = !$passwordIsConfirmed;
+    }
+    else
+    {
+       $add_account_stmt->execute(array($username, $fullName, $email,$password));
+       header('Location: index.php');
+    }
+}
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
@@ -37,6 +65,14 @@ include('connection.php');
 					<li>
 						<label for="confirm_pwd">Confirm Password</label>
 						<input type="password" name="confirm_pwd" id="confirm_pwd" required />
+                        <p class="error">
+                            <?php
+                        if($notConfirmed)
+                        {
+                            print_r("Please confirm password carefully");
+                        }
+                        ?>
+                        </p>
 					</li>
 					<li>
 						<input type="submit" value="Submit" /> &nbsp; Already registered? <a href="index.php">Login</a>
